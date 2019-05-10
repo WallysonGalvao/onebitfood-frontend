0dcc45dbb2ce52aa13de7d121f0300734402aeec
Wallyson Galvão <wallyson.galvao@gmail.com>
2019-05-10 16:11:34 -0300

    Lista de restaurantes com redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,14 @@
-import { combineReducers } from 'redux';
-import RestaurantsRducer from './restaurantsReducer';
+import { combineReducers } from "redux";
+import RestaurantsReducer from "./restaurantsReducer";
+/* import orderReducer from "./orderReducer";
+import modalReducer from "./modalReducer";
+import addressReducer from "./addressReducer";
+import newOrderReducer from "./newOrderReducer"; */
 
 export default combineReducers({
-    restaurantsState: RestaurantsRducer
-});
+    restaurantsState: RestaurantsReducer,
+    /* orderState: orderReducer,
+    modalState: modalReducer,
+    addressState: addressReducer,
+    newOrderState: newOrderReducer */
+})
